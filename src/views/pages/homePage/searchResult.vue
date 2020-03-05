ec7c45672364e4179aa45a0c1a32d350866c99f8
zbljbl <13561742520@163.com>
2020-03-05 22:48:14 +0800

    完成搜索结果页

--- src/views/pages/homePage/searchResult.vue
+++ src/views/pages/homePage/searchResult.vue
@@ -22,7 +22,10 @@
             界面相材料晶体结构：
         </div>
         <div style="overflow:hidden">
-            <div class="fl crystalImg"></div>
+            <div class="fl crystalImg">
+                <img class="fangda" src="../../../assets/images/fangdafff.png" alt="">
+                <img class="xiazai" src="../../../assets/images/downicon.png" alt="">
+            </div>
             <div class="fl crystalParameter">
                 <div class="parameterTit">品格参数</div>
                 <div class="tableBorer">
@@ -70,8 +73,14 @@
             <div class="fl parameterTit">电子能态密度</div>
         </div>
         <div class="erpBigBox">
-            <div class="fl" style="border-right:1px solid #ddd;"><img src="../../../assets/images/take.png" alt=""></div>
-            <div class="fl"><img src="../../../assets/images/take.png" alt=""></div>
+            <div class="fl" style="border-right:1px solid #ddd;">
+                <img class="fangda" src="../../../assets/images/fangda000.png" alt="">
+                <img src="../../../assets/images/take.png" alt="">
+            </div>
+            <div class="fl">
+                <img class="fangda" src="../../../assets/images/fangda000.png" alt="">
+                <img src="../../../assets/images/take.png" alt="">
+            </div>
         </div>
         <div class="subfield" style="margin-top:42px">
             <i></i>
@@ -84,13 +93,13 @@
         <div class="clearFloat clearFloat">
             <div class="fl matrixBigBox">
                 <div class="matrixMask"></div>
-                <div class="matrixbox">
+                <div class="matrixbox borderTL">
                     <div class="matrixRowBox" v-for="(item, index) in matrixData" :key="index">
                         <div v-for="(data, index2) in item" :key="index2">{{data}}</div>
                     </div>
                 </div>
             </div>
-            <div class="fl matrixDataBigBox">
+            <div class="fl matrixDataBigBox borderTL">
                 <div class="matrixRSmallBox fl" v-for="(item, index) in matrixRightData" :key="index">
                     <div class="matrixBgBox" >
                         <span>{{item.name}}</span>
@@ -105,8 +114,8 @@
             <i></i>
             界面相合成：
         </div>
-        <div class="hechengBigBox">
-            <div class="downIcon"><i></i></div>
+        <div class="hechengBigBox borderTL">
+            <div class="downIcon"><img src="../../../assets/images/downPull.png" alt=""></div>
             <div><span>界面相合成方法</span>TG-CVI, "Thermal Gradient Chemical vapour infiltration"</div>
             <div><span>界面相合成热处理温度</span>1000 °C</div>
             <div><span>界面相合成原料</span>BF3-NH3-H2</div>
@@ -115,17 +124,118 @@
             <i></i>
             界面相结构和成分：
         </div>
-        <div>这有个表</div>
+        <div class="chengfenTable borderTL">
+            <ul class="chengfenTitle clearFloat">
+                <li>
+                    <span style="right:4px;top:4px">成分</span>
+                    <span style="left:4px;bottom:4px">表征方法</span>
+                    <span class="hr"></span>
+                </li>
+                <li>B</li><li>N</li><li>O</li><li>--</li><li>--</li>
+            </ul>
+            <ul class="chengfenTrs clearFloat" v-for="(data,index) in chenfenTable" :key="index">
+                <li>{{data.name}}</li>
+                <li v-for="(item,i) in data.data" :key="i">{{item}}</li>
+            </ul>
+        </div>
         <div class="erpTit">
             <div class="fl parameterTit" style="border-right:1px solid #fff;width:33%">XRD图谱</div>
             <div class="fl parameterTit" style="border-right:1px solid #fff;width:33%">拉曼吸收峰</div>
             <div class="fl parameterTit" style="width:33%">红外吸收峰</div>
         </div>
         <div class="erpBigBox">
-            <div class="fl" style="border-right:1px solid #ddd;width:33%"><img src="../../../assets/images/take.png" alt=""></div>
-            <div class="fl" style="border-right:1px solid #ddd;width:33%"><img src="../../../assets/images/take.png" alt=""></div>
-            <div class="fl" style="width:33%"><img src="../../../assets/images/take.png" alt=""></div>
+            <div class="fl" style="border-right:1px solid #ddd;width:33%"><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/take.png" alt=""></div>
+            <div class="fl" style="border-right:1px solid #ddd;width:33%"><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/take.png" alt=""></div>
+            <div class="fl" style="width:33%"><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/take.png" alt=""></div>
+        </div>
+        <div class=" parameterTit" style="border-right:1px solid #fff;margin-top:20px">界面相形貌</div>
+        <div class="xingmaoBorder">
+            <div class="hechengBigBox borderTL">
+                <div><span>界面相厚度</span>0.1~5 μm</div>
+                <div><span>界面相形貌</span>Turbostratic structure (t-BN)</div>
+            </div>
+            <div class="borderTL imgDatailsList">
+                <ul class="clearFloat">
+                    <li><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/img123.png" alt=""></li>
+                    <li><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/img123.png" alt=""></li>
+                    <li><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/img123.png" alt=""></li>
+                    <li class="nomore"><img src="../../../assets/images/nomore.png" alt=""></li>
+                    <li class="nomore"><img src="../../../assets/images/nomore.png" alt=""></li>
+                </ul>
+            </div>
+        </div>
 
+        <div class="subfield" style="margin-top:42px">
+            <i></i>
+            复合材料结构和成分:
+        </div>
+        <div class="hechengBigBox borderTL">
+            <div><span>复合材料类型</span>2D composite</div>
+            <div><span>基体</span>SiC fiber, Su ZhouSaiLiFei (SLF) Ceramic Fiber Co., LTD, China.</div>
+            <div><span>纤维成分</span>SiC, CVI</div>
+            <div><span>纤维型号</span>SiC, CVI</div>
+        </div>
+
+        <div class="subfield" style="margin-top:42px">
+            <i></i>
+            界面力学性能：
+        </div>
+        <div class="hechengBigBox borderTL">
+            <div class="downIcon"><img src="../../../assets/images/downPull.png" alt=""></div>
+            <div><span>界面剪切应力</span>329.91 MPa</div>
+        </div>
+        <div class="borderTL imgDatailsList" style="margin-top:50px;">
+            <ul class="clearFloat">
+                <li><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/img123.png" alt=""></li>
+                <li><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/img123.png" alt=""></li>
+                <li><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/img123.png" alt=""></li>
+                <li class="nomore"><img src="../../../assets/images/nomore.png" alt=""></li>
+                <li class="nomore"><img src="../../../assets/images/nomore.png" alt=""></li>
+            </ul>
+        </div>
+
+        <div class="subfield" style="margin-top:42px">
+            <i></i>
+            复合材料力学性能：
+        </div>
+        <div class="hechengBigBox borderTL">
+            <div class="downIcon"><img src="../../../assets/images/downPull.png" alt=""></div>
+            <div><span>拉伸强度</span>329.91 MPa</div>
+            <div><span>剪切强度</span>329.91 MPa</div>
+            <div><span>界面剪切应力</span>329.91 MPa</div>
+            <div><span>拉伸失效应变</span>329.91 MPa</div>
+            <div><span>拉伸失效应力</span>329.91 MPa</div>
+        </div>
+        <div class="borderTL imgDatailsList" style="margin-top:50px;">
+            <ul class="clearFloat">
+                <li><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/img123.png" alt=""></li>
+                <li><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/img123.png" alt=""></li>
+                <li><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/img123.png" alt=""></li>
+                <li class="nomore"><img src="../../../assets/images/nomore.png" alt=""></li>
+                <li class="nomore"><img src="../../../assets/images/nomore.png" alt=""></li>
+            </ul>
+        </div>
+
+        <div class="subfield" style="margin-top:42px">
+            <i></i>
+            复合材料抗氧化性能：
+        </div>
+        <div class="hechengBigBox borderTL">
+            <div class="downIcon"><img src="../../../assets/images/downPull.png" alt=""></div>
+            <div><span>氧化材料抗氧化性能</span>329.91 MPa</div>
+            <div><span>氧化处理温度</span>329.91 MPa</div>
+            <div><span>氧化处理时间</span>329.91 MPa</div>
+            <div><span>氧化处理气氛</span>329.91 MPa</div>
+            <div><span>氧化后残余弯曲强度</span>329.91 MPa</div>
+        </div>
+        <div class="borderTL imgDatailsList" style="margin-top:50px;margin-bottom:250px">
+            <ul class="clearFloat">
+                <li><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/img123.png" alt=""></li>
+                <li><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/img123.png" alt=""></li>
+                <li><img class="fangda" src="../../../assets/images/fangda000.png" alt=""><img src="../../../assets/images/img123.png" alt=""></li>
+                <li class="nomore"><img src="../../../assets/images/nomore.png" alt=""></li>
+                <li class="nomore"><img src="../../../assets/images/nomore.png" alt=""></li>
+            </ul>
         </div>
     </div>
 </template>
@@ -135,6 +245,13 @@ export default {
     name: 'searchResult',
     data() {
         return {
+            chenfenTable:[
+                {name:'名义',data:['--','--','--','--','--']},
+                {name:'XPS',data:['23','45','54','--','--']},
+                {name:'AES',data:['23','34','55','--','--']},
+                {name:'EELS',data:['23','56','34','--','--']},
+                {name:'--',data:['--','--','--','--','--']},
+            ],
             tabPosition:'left',
             tableData:[
                 {x:'1',y:'2',z:'3'},
@@ -166,7 +283,7 @@ export default {
         }
     },
     created() {
-
+        console.log(this.chenfenTable)
     },
     methods: {
 
@@ -201,10 +318,112 @@ export default {
     }
 </style>
 <style scoped>
+    .imgDatailsList{
+        margin-top: 20px;
+        height: 409px;
+        overflow: hidden;
+    }
+    .imgDatailsList img{
+        width: 100%;
+        height: auto;
+    }
+    .imgDatailsList .fangda{
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        width: auto;
+    }
+    .nomore img{
+        width: 148px;
+        height: auto;
+        position: absolute;
+        top: 50%;
+        transform:translate(-50%,-50%);
+        left: 50%;
+    }
+    .imgDatailsList li{
+        position: relative;
+        border-right: 1px solid #ddd;
+        border-bottom: 1px solid #ddd;
+        width: 233px;
+        height: 100%;
+        float: left;
+        padding-top: 60px;
+        text-align:center;
+    }
+    .imgDatailsList ul{
+        width: 1205px;
+        height: 100%;
+    }
+    .borderTL{
+        border-top: 1px solid #ddd;
+        border-left: 1px solid #ddd;
+    }
+    .xingmaoBorder{
+        border: 1px solid #ddd;
+        border-top: none;
+        padding: 21px 16px 23px 16px;
+    }
+    .chengfenTrs li{
+        border-right: 1px solid #ddd;
+        border-bottom: 1px solid #ddd;
+        height: 40px;
+        line-height: 40px;
+        width: 211px;
+        text-align: center;
+        font-size: 14px;
+        color: #4d4d4d;
+        float: left;
+    }
+    .chengfenTrs>:first-child{
+        width: 143px;
+        background: #E2FCF9;
+        color: #4D4D4D;
+        position: relative;
+    }
+    .chengfenTitle .hr{
+        width: 120%;
+        border-top: 1px solid #fff;
+        height: 0;
+        left: 0;
+        top: 0;
+        transform: rotate(20deg);
+        transform-origin:top left;
+    }
+    .chengfenTitle span{
+        position: absolute;
+        line-height: 20px!important;
+        font-size: 16px;
+    }
+    .chengfenTitle{
+        height: 53px;
+        line-height: 53px;
+    }
+    .chengfenTitle li{
+        text-align: center;
+        height: 53px;
+        line-height: 53px;
+        border-bottom: 1px solid #ddd;
+        border-right:1px solid #ddd;
+        width: 211px;
+        float: left;
+        background: #E2FCF9;
+        color: #4d4d4d;
+        font-size: 14px;
+        font-weight: bold;
+    }
+    .chengfenTitle>:first-child{
+        width: 143px;
+        background: #33B0B5;
+        color: #fff;
+        position: relative;
+    }
+    .chengfenTable{
+        margin-bottom: 20px;
+    }
     .downIcon{
         width: 60px;
         height: 30px!important;
-        border: 1px solid #ddd;
         border-top: none;
         text-align: center;
         position: absolute;
@@ -214,8 +433,6 @@ export default {
     }
     .hechengBigBox{
         position: relative;
-        border-top: 1px solid #ddd;
-        border-left: 1px solid #ddd;
     }
     .hechengBigBox>div{
         height: 40px;
@@ -258,8 +475,6 @@ export default {
         width: 792px;
         height: 360px;
         margin-left: 24px;
-        border-top: 1px solid #ddd;
-        border-left: 1px solid #ddd;
     }
     .matrixRowBox{
         width:360px;
@@ -294,8 +509,6 @@ export default {
         top: 8px;
         height: 360px;
         width: 360px;
-        border-top: 1px solid #ddd;
-        border-left: 1px solid #ddd;
         z-index: 3
     }
     .clearFloat{
@@ -320,6 +533,12 @@ export default {
         transform: translate(-50%,-50%);
         max-width: 100%;
         height: auto;
+    }
+    .erpBigBox .fangda{
+        transform:translate(0,0)!important;
+        right: 10px!important;
+        top: 10px!important;
+        left:unset
     }
     .erpTit{
         overflow: hidden;
@@ -374,6 +593,18 @@ export default {
         background: #000;
         border-radius: 10px;
         margin-right: 37px;
+        position: relative;
+    }
+    .crystalImg img{
+        position: absolute;
+    }
+    .crystalImg .fangda{
+        top: 24px;
+        right: 24px;
+    }
+    .crystalImg .xiazai{
+        bottom: 24px;
+        right: 24px;
     }
     .subfield{
         border-bottom: 1px dashed rgba(204,204,204,1);
